332930a0cb3f803fe166472319323cdc7b7b0fe7
GGilley20 <90412804+GGilley20@users.noreply.github.com>
2021-11-30 14:26:27 -0500

    v 0.2
    setup the game window

--- .github/myFirstPygame.py
+++ .github/myFirstPygame.py
@@ -1,4 +1,12 @@
-# My First PyGame, Gabriel Gilley, 11/30/21, 2:19PM, v 0.1
+# My First PyGame, Gabriel Gilley, 11/30/21, 2:25PM, v 0.2
 
 import pygame, sys
 from pygame.locals import *
+
+# Start PyGame
+pygame.init()
+
+# Setup the game window.
+windowSurface = pygame.display.set_mode((500, 400), 0, 32)
+pygame.display.set_caption('Hello, world!')
+
