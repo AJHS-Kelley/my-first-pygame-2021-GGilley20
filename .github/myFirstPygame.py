6ecf35edcc489a9da235d76f5338a9dd1c1e5d1d
GGilley20 <90412804+GGilley20@users.noreply.github.com>
2021-11-30 14:37:13 -0500

    V 0.3
    Setup Game Color

--- .github/myFirstPygame.py
+++ .github/myFirstPygame.py
@@ -1,4 +1,4 @@
-# My First PyGame, Gabriel Gilley, 11/30/21, 2:25PM, v 0.2
+# My First PyGame, Gabriel Gilley, 11/30/21, 2:36PM, v 0.3
 
 import pygame, sys
 from pygame.locals import *
@@ -10,3 +10,10 @@ pygame.init()
 windowSurface = pygame.display.set_mode((500, 400), 0, 32)
 pygame.display.set_caption('Hello, world!')
 
+# Setup color values
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+LAVISHPURPLE = (129, 66, 212)
